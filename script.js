939cc816018c23acf83dc47c028774e02e78e8ae
phoz2425 <166633133+phoz2425@users.noreply.github.com>
2024-04-19 02:48:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -32,15 +32,6 @@ var firebaseConfig = {
 
 var database;
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
 function saveGameResults() {
     var gameResultsRef = ref(database, 'gameResults/');
     push(gameResultsRef, gameResults)
